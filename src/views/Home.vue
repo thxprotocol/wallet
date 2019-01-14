ac5211465856253e265e239f12124baca0b15ade
Peter Polman <peter@peterpolman.nl>
2019-01-14 21:30:03 +0100

    Removed old files, restructed home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,38 +13,25 @@ export default {
   props: {
     providerURL: "http://localhost:8545"
   },
-  data: function () {
-    return {
-      balance: 0
-    }
-  },
-  mounted() {
-    this.init()
-  },
   methods: {
     init() {
       if (typeof web3 !== 'undefined') {
         web3 = new Web3(web3.currentProvider);
       } else {
-        // Set the provider you want from Web3.providers
         web3 = new Web3(new Web3.providers.HttpProvider(this.providerURL))
       }
 
       console.log(`Running web3 version: ${web3.version.api}`)
 
-      // Let's request the balance for your MetaMask account
+      // Let's request the balance for the MetaMask account
       const accounts = web3.eth.accounts
 
       this.getBalance(accounts[0]).then((result) => {
         // Since this method returns a BigNumber convert it to a Number first
         var balanceInWei = result.toNumber()
 
-        // Calculate the amount of ETH represented by this
-        // Number, be careful! a string is returned:)
-        var balanceInETH = web3.fromWei(balanceInWei, 'ether')
-
-        // Populate the Vue prop so the view is automagically updated.
-        this.balance = balanceInETH
+        // Calculate the amount of ETH represented by this Number, be careful! a string is returned:)
+        this.balance = web3.fromWei(balanceInWei, 'ether')
       });
 
     },
@@ -61,7 +48,14 @@ export default {
         })
       })
     }
+  },
+  data: function () {
+    return {
+      balance: 0
+    }
+  },
+  mounted() {
+    this.init()
   }
-
 }
 </script>
