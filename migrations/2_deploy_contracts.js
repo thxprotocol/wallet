bc731db1d97ecfb98d5d07eefc71f8cb13c351d1
Peter Polman <peter@peterpolman.nl>
2019-01-14 15:45:08 +0100

    Updated installation instructions, updated truffle scripts

--- migrations/2_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-const THXToken = artifacts.require("THXToken");
-
-module.exports = function(deployer, network, accounts) {
-    deployer.deploy(THXToken, _listener = '', _owners = [], manager = accounts[0]);
-};
