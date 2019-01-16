b802fce7128c2144ce8074ee4795417f56173b3c
Peter Polman <peter@peterpolman.nl>
2019-01-16 15:27:46 +0100

    Refactor and fixed issue with static contract address in mint method

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,43 +24,44 @@ export default {
     }
   },
   methods: {
-    init() {
+    async init() {
       if (typeof web3 !== 'undefined') {
         web3 = new Web3(web3.currentProvider);
       } else {
         web3 = new Web3(new Web3.providers.HttpProvider(this.providerURL))
       }
 
+      // Get the network id
+      const nid = await web3.eth.net.getId()
 
-      // Set up an async method that retrieves the accounts on the network
-      web3.eth.getAccounts(async (error, accounts) => {
-        // Get the network id
-        const nid = await web3.eth.net.getId()
+      // Create an instance from the THXToken Contract abi and contract address on the current network
+      this.thxTokenInstance = new web3.eth.Contract(THXTokenJSON.abi, THXTokenJSON.networks[nid].address)
 
-        // Create an instance from the THXToken Contract abi and contract address on the current network
-        let THXTokenInstance = new web3.eth.Contract(THXTokenJSON.abi, THXTokenJSON.networks[nid].address)
-
+      // Retrieve the accounts available on the network
+      await web3.eth.getAccounts((error, accounts) => {
         // Store the accounts
         this.accounts = accounts
-
-        // Retrieve the amount of Wei for account 0
-        let amountInWei = await web3.eth.getBalance(this.accounts[0])
-
-        // Calculate the amount of ETH from Wei
-        this.balanceETH = web3.utils.fromWei(amountInWei, 'ether')
-
-        // Retrieve the amount of THX for account 0
-        this.balanceTHX = await THXTokenInstance.methods.balanceOf(this.accounts[0]).call()
       })
-    },
-    async onClickMint() {
-      let THXTokenInstance = new web3.eth.Contract(THXTokenJSON.abi, "0x0AA6728c7844c7a5c8fB0fd078FD6F0254FB60E8")
 
-      // Mint 1000 THX for account 0. Make sure to call from account 0 that has the MinterRole
-      await THXTokenInstance.methods.mint(this.accounts[0], 1000).send({from: this.accounts[0]})
+      // Update the balances for account 0
+      this.updateBalances(this.accounts[0])
+    },
+    async updateBalances(account) {
+      // Retrieve the amount of Wei for account 0
+      let amountInWei = await web3.eth.getBalance(account)
+
+      // Calculate the amount of ETH from Wei
+      this.balanceETH = web3.utils.fromWei(amountInWei, 'ether')
 
       // Retrieve the amount of THX for account 0
-      this.balanceTHX = await THXTokenInstance.methods.balanceOf(this.accounts[0]).call()
+      this.balanceTHX = await this.thxTokenInstance.methods.balanceOf(account).call()
+    },
+    async onClickMint() {
+      // Mint 1000 THX for account 0. Make sure to call from account 0 that has the MinterRole
+      await this.thxTokenInstance.methods.mint(this.accounts[0], 1000).send({from: this.accounts[0]})
+
+      // Update the balances for account 0
+      this.updateBalances(this.accounts[0])
     }
   },
   mounted() {
