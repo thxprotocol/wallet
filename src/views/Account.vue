4c4ccbf8e60f43c2a8aa22fa9e0effcc922a9c36
Peter Polman <peter@peterpolman.nl>
2019-01-20 15:17:26 +0100

    Cleanup

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -15,13 +15,6 @@
         <button class="btn btn--default" type="submit">Deposit {{ transferToPoolAmount }} THX</button>
       </form>
 
-      <h3>Submit reward:</h3>
-      <form v-on:submit="submitReward()">
-        <input v-model="rewardSlug" type="text" placeholder="reward_type" />
-        <input v-model="rewardAmount" type="number" min="0" v-bind:max="balance.pool" />
-        <button class="btn btn--default" type="submit">Submit Reward!</button>
-      </form>
-
     </main>
   </article>
 </template>
@@ -64,7 +57,6 @@ export default {
     onMintForAccount() {
       const token = this.network.instances.token
 
-      // Mint 1000 THX for account 0. Make sure to call from account 0 that has the MinterRole
       return token.methods.mint(this.network.accounts[0], this.mintForAccountAmount).send({from: this.network.accounts[0]}).then(() => {
         return this.$refs.header.updateBalance()
       })
@@ -75,17 +67,7 @@ export default {
       return pool.methods.deposit(this.transferToPoolAmount).send({from: this.network.accounts[0]}).then(() => {
         return this.$refs.header.updateBalance()
       })
-
-      // const token = this.network.instances.token;
-      // return token.methods.transfer(this.network.addresses.pool, this.transferToPoolAmount).send({from: this.network.accounts[0]}).then(() => {
-      //   return this.$refs.header.updateBalance()
-      // })
-    },
-    submitReward() {
-      const pool = this.network.instances.pool;
-
-      return pool.methods.add(this.rewardSlug, this.rewardAmount).send({from: this.network.accounts[0]})
-    },
+    }
   }
 }
 </script>
--- src/views/Reward.vue
+++ src/views/Reward.vue
@@ -106,7 +106,7 @@ export default {
       }
 
       return rewardIds;
-    },
+    }
   }
 }
 </script>
