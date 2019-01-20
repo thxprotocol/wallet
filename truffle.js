9fa70a45ed404047f119f089ad5eb27fac78aef2
Sjoerd van der Vis <sjoerdvandervis@goalgorilla.com>
2019-01-20 14:28:54 +0100

    Added dotenv for variables to be used for a Ropsten deploy and adapted truffle.js for this.

--- truffle.js
+++ truffle.js
@@ -1,3 +1,6 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+require('dotenv').config();
+
 module.exports = {
     networks: {
         ganache: {
@@ -9,6 +12,13 @@ module.exports = {
             host: "localhost",
             port: 7545,
             network_id: "*"
+        },
+        ropsten: {
+            provider: function() {
+                return new HDWalletProvider(process.env.MNEMONIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY)
+            },
+            network_id: 3,
+            gas: 2900000
         }
     },
     solc: {
