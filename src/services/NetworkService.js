70044dc5e187bcc40d32aa8a9da58fec269b3f85
Peter Polman <peter@peterpolman.nl>
2019-07-23 15:23:38 +0200

    Upgraded to correct secure websocket urls

--- src/services/NetworkService.js
+++ src/services/NetworkService.js
@@ -21,13 +21,13 @@ export default class NetworkService {
     _createClient(key) {
         this.privateKey = CryptoUtils.B64ToUint8Array(key);
         this.publicKey = CryptoUtils.publicKeyFromPrivateKey(this.privateKey);
-        let writeUrl = 'wss://127.0.0.1:46658/websocket'
-        let readUrl = 'wss://127.0.0.1:46658/queryws'
+        let writeUrl = 'ws://127.0.0.1:46658/websocket'
+        let readUrl = 'ws://127.0.0.1:46658/queryws'
         let networkId = 'default'
 
         if (process.env.NETWORK == 'extdev') {
-            writeUrl = 'ws://extdev-plasma-us1.dappchains.com:80/websocket'
-            readUrl = 'ws://extdev-plasma-us1.dappchains.com:80/queryws'
+            writeUrl = 'wss://extdev-plasma-us1.dappchains.com/websocket'
+            readUrl = 'wss://extdev-plasma-us1.dappchains.com/queryws'
             networkId = 'extdev-plasma-us1'
         }
 
