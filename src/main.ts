d1e2c05e838d45dc88d12b0a1afed0bd3e510023
Peter Polman <peter@peterpolman.nl>
2020-08-04 11:04:28 +0200

    [THX-215] Matic.js POC

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,9 @@ import { ModalPlugin } from 'bootstrap-vue';
 import UserService from '@/services/UserService';
 import PoolService from '@/services/PoolService';
 
+import Matic from '@maticnetwork/maticjs';
+import HDWalletProvider from '@truffle/hdwallet-provider';
+
 Vue.use(VueTimers);
 Vue.config.productionTip = false;
 Vue.use(VueMoment);
@@ -23,7 +26,7 @@ Vue.use(ModalPlugin);
 let app: any;
 
 firebase.initializeApp(config.firebase[process.env.NODE_ENV as any]);
-firebase.auth().onAuthStateChanged((user: firebase.User | any = firebase.auth().currentUser) => {
+firebase.auth().onAuthStateChanged(async (user: firebase.User | any = firebase.auth().currentUser) => {
     if (user) {
         const stateService: StateService = new StateService(user.uid);
         const poolService = new PoolService();
@@ -45,6 +48,59 @@ firebase.auth().onAuthStateChanged((user: firebase.User | any = firebase.auth().
                 'It looks like you misconfigured your extdev private key. Provide it through the accounts page.',
             );
         }
+
+        let tx, mumbaiBalance, goerliBalance;
+        // const GOERLI_ACCOUNT = '0xF986ed06Bc54227e5E28E6aC93F34fc0Fac497eB';
+        // const GOERLI_TOKEN = '0x3f152B63Ec5CA5831061B2DccFb29a874C317502';
+        const GOERLI_TOKEN = '0x24EB81d800786fFAD0F26611868B8d8a02303790';
+        const MUMBAI_ACCOUNT = '0x86C0F290b7bdf02946D115c00a0A6ae543379544';
+        // const MUMBAI_TOKEN = '0x2d7882beDcbfDDce29Ba99965dd3cdF7fcB10A1e';
+        const MUMBAI_TOKEN = '0x6EC9cF44A185102b9A312dae7C8f6D8961C0E020';
+        const amount = 1000000000000000000;
+        const parentProvider = new HDWalletProvider(
+            '0x4a5f684bade60056c0cc23c7ec55dd7bc283c5530e61ef04930a00b05636f897',
+            'https://goerli.infura.io/v3/350a0215b02e46639ff2ac1982de4aed',
+        );
+        const maticProvider = new HDWalletProvider(
+            '0x3445ebd670b5402e710f68425c9f5d10de4d59e7f6dd09bfd24144efa2e4d4cf',
+            'https://rpc-mumbai.matic.today',
+        );
+        const matic = new Matic({
+            parentProvider,
+            maticProvider,
+            parentDefaultOptions: {
+                from: MUMBAI_ACCOUNT,
+            },
+            maticDefaultOptions: {
+                from: MUMBAI_ACCOUNT,
+            },
+            posRootChainManager: '0x2890bA17EfE978480615e330ecB65333b880928e',
+        });
+
+        matic.initialize();
+
+        async function getBalances() {
+            mumbaiBalance = await matic.balanceOfERC20(MUMBAI_ACCOUNT, MUMBAI_TOKEN, { parent: false });
+            goerliBalance = await matic.balanceOfERC20(MUMBAI_ACCOUNT, GOERLI_TOKEN, { parent: true });
+
+            console.log(`Goerli Balance:`, goerliBalance);
+            console.log(`Mumbai Balance:`, mumbaiBalance);
+        }
+
+        await getBalances();
+        debugger;
+        tx = await matic.approveERC20TokensForDeposit(GOERLI_TOKEN, amount);
+        console.log(`TX ERC20 Approve on Goerli:`, tx);
+
+        tx = await matic.depositERC20ForUser(GOERLI_TOKEN, MUMBAI_ACCOUNT, amount, {
+            from: MUMBAI_ACCOUNT,
+            gasPrice: '10000000000',
+        });
+        console.log(`TX ERC20 Deposit on Goerli:`, tx);
+
+        await getBalances();
+
+        debugger;
     } else {
         Vue.prototype.$network = new NetworkService();
     }
--- src/types/web3.d.ts
+++ src/types/web3.d.ts
@@ -1,1 +1,3 @@
 declare module 'web3';
+declare module '@maticnetwork/maticjs';
+declare module '@maticnetwork/meta/network';
