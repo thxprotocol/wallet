eabba684c6e618f7975a0cf198801f0d5832d134
Peter Polman <peter@peterpolman.nl>
2021-05-04 12:18:19 +0200

    Adds GTM to auth views

--- src/App.vue
+++ src/App.vue
@@ -31,5 +31,21 @@ import Navbar from '@/components/Navbar.vue';
         profile: 'account/profile',
     }),
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+    created() {
+        (function(w: any, d, s, l: any, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                'event': 'gtm.js',
+            });
+            const f: any = d.getElementsByTagName(s)[0],
+                j: any = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', process.env.VUE_APP_GTM);
+    }
+}
 </script>
