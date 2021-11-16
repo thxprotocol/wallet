7df11fb535252fa7e14723436b1248e2f225bcc4
Peter Polman <peter@peterpolman.nl>
2021-11-16 11:43:41 +0100

    Limit oidc scopes

--- src/utils/oidc.ts
+++ src/utils/oidc.ts
@@ -6,13 +6,10 @@ export const config: any = {
     client_secret: OIDC_CLIENT_SECRET, // eslint-disable-line @typescript-eslint/camelcase
     redirect_uri: `${BASE_URL}/signin-oidc`, // eslint-disable-line @typescript-eslint/camelcase
     response_type: 'code', // eslint-disable-line @typescript-eslint/camelcase
-
     id_token_signed_response_alg: 'RS256', // eslint-disable-line @typescript-eslint/camelcase
     post_logout_redirect_uri: BASE_URL, // eslint-disable-line @typescript-eslint/camelcase
-
     silent_redirect_uri: `${BASE_URL}/silent-renew`, // eslint-disable-line @typescript-eslint/camelcase
     automaticSilentRenew: true,
-
-    loadUserInfo: true,
-    scope: 'openid user email offline_access',
+    loadUserInfo: false,
+    scope: 'openid user',
 };
