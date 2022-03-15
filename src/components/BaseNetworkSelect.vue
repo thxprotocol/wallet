a06e80ae84cd7b41f4b778c64a5d43298e32fb43
Peter Polman <peter@peterpolman.nl>
2022-03-15 16:50:04 +0100

    Add native token balances to dropdown

--- src/components/BaseNetworkSelect.vue
+++ src/components/BaseNetworkSelect.vue
@@ -1,5 +1,5 @@
 <template>
-    <b-dropdown size="sm" variant="darker" no-caret>
+    <b-dropdown size="sm" variant="darker" no-caret v-if="profile">
         <template #button-content>
             <div class="d-flex align-items-center">
                 <i class="fas fa-code-branch p-1 mr-2 text-success" style="font-size: 1.5rem"></i>
@@ -8,8 +8,15 @@
                 </span>
             </div>
         </template>
-        <b-dropdown-item> <i class="fas fa-code-branch text-muted"></i> Polygon Test </b-dropdown-item>
-        <b-dropdown-item> <i class="fas fa-code-branch text-success"></i> Polygon Main </b-dropdown-item>
+        <b-dropdown-item target="_blank" :href="`https://mumbai.polygonscan.com/address/${profile.address}`">
+            <i class="fas fa-code-branch text-muted mr-2"></i> Polygon Mumbai<br />
+            <small class="text-muted">MATIC: {{ balances[0] }}</small>
+        </b-dropdown-item>
+        <b-dropdown-divider />
+        <b-dropdown-item target="_blank" :href="`https://polygonscan.com/address/${profile.address}`">
+            <i class="fas fa-code-branch text-success mr-2"></i> Polygon Mainnet<br />
+            <small class="text-muted">MATIC: {{ balances[1] }}</small>
+        </b-dropdown-item>
     </b-dropdown>
 </template>
 <script lang="ts">
@@ -24,10 +31,14 @@ import { fromWei } from 'web3-utils';
     computed: mapGetters({
         profile: 'account/profile',
         privateKey: 'account/privateKey',
+        networks: 'network/all',
     }),
 })
 export default class BaseNetworkSelect extends Vue {
-    balances!: { [NetworkProvider.Test]: number; [NetworkProvider.Main]: number };
+    balances: { [NetworkProvider.Test]: number; [NetworkProvider.Main]: number } = {
+        [NetworkProvider.Test]: 0,
+        [NetworkProvider.Main]: 0,
+    };
 
     // getters
     profile!: UserProfile;
@@ -40,6 +51,9 @@ export default class BaseNetworkSelect extends Vue {
         }
         await this.$store.dispatch('network/setNetwork', { npid: NetworkProvider.Test, privateKey: this.privateKey });
         await this.$store.dispatch('network/setNetwork', { npid: NetworkProvider.Main, privateKey: this.privateKey });
+
+        await this.getBalance(NetworkProvider.Test);
+        await this.getBalance(NetworkProvider.Main);
     }
 
     async getBalance(npid: NetworkProvider) {
