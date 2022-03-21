2b5e2d8ebdff4862f37aa870634b2c73dd9a3155
Bram Rongen <bram@thx.network>
2022-03-21 15:24:14 +0100

    Remove unused PWA config

--- vue.config.js
+++ vue.config.js
@@ -4,30 +4,5 @@ module.exports = {
         https: true,
         port: 8083,
     },
-    pwa: {
-        name: 'THX Web Wallet',
-        themeColor: '#FDE542',
-        iconPaths: {
-            favicon32: 'img/icons/pwa_icon.png',
-            favicon16: 'img/icons/pwa_icon.png',
-            appleTouchIcon: 'img/icons/pwa_icon.png',
-            msTileImage: 'img/icons/pwa_icon.png',
-        },
-        manifestOptions: {
-            short_name: 'THX',
-        },
-        display: 'standalone',
-        msTileColor: '#000000',
-        appleMobileWebAppCapable: 'yes',
-        appleMobileWebAppStatusBarStyle: 'black',
-
-        // configure the workbox plugin
-        workboxPluginMode: 'InjectManifest',
-        workboxOptions: {
-            // swSrc is required in InjectManifest mode.
-            swSrc: 'dev/sw.js',
-            // ...other Workbox options...
-        },
-    },
     outputDir: './dist/',
 };
