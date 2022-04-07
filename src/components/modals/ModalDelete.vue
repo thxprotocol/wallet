ad22b36ab66651567c9327ec9c1b3b59dacc143c
GarfDev <garfdev.13@gmail.com>
2022-04-07 18:55:54 +0700

    fix: resolve image error

--- src/components/modals/ModalDelete.vue
+++ src/components/modals/ModalDelete.vue
@@ -26,7 +26,7 @@
                     `
                     border-top-left-radius: 0.5rem;
                     border-top-right-radius: 0.5rem;
-                    background-image: url(${require('../assets/thx_modal-header-danger.webp')});
+                    background-image: url(${require('../../assets/img/thx_modal-header-danger.webp')});
                 `
                 "
             >
