6aeaa538a252f554eebba7e0ce925e8096ba23f5
GarfDev <garfdev.13@gmail.com>
2022-04-07 18:36:40 +0700

    fix: resolve commented issues

--- src/components/BaseListGroupItemAssetPool.vue
+++ src/components/BaseListGroupItemAssetPool.vue
@@ -1,10 +1,20 @@
 <template>
     <b-list-group-item
         :to="`/memberships/${membership.id}/withdrawals`"
-        v-if="membership && membership.token"
         class="d-flex justify-content-between align-items-center"
     >
-        <div class="mr-auto">
+        <div v-if="!membership.token" class="mr-auto">
+            <i
+                class="fas fa-code-branch mr-2"
+                :class="{ 'text-success': membership.network, 'text-muted': !membership.network }"
+            ></i>
+            <strong v-if="!membership.token" class="text-danger mr-1">This Pool deleted by it owner</strong>
+            <small v-if="!membership.token" class="text-muted text-overflow-75">
+                {{ membership.id }}
+            </small>
+        </div>
+
+        <div v-if="membership.token" class="mr-auto">
             <i
                 class="fas fa-code-branch mr-2"
                 :class="{ 'text-success': membership.network, 'text-muted': !membership.network }"
@@ -15,7 +25,9 @@
             <small class="text-muted text-overflow-75">{{ membership.poolAddress }}</small>
         </div>
 
-        <div class="h3 mr-3 m-0">{{ membership.token.balance }} {{ membership.token.symbol }}</div>
+        <div v-if="membership.token" class="h3 mr-3 m-0">
+            {{ membership.token.balance }} {{ membership.token.symbol }}
+        </div>
         <b-dropdown variant="white" no-caret toggle-class="d-flex align-items-center" v-if="profile">
             <template #button-content>
                 <i class="fas fa-ellipsis-v p-1 ml-0 text-muted" aria-hidden="true" style="font-size: 1rem"></i>
@@ -24,9 +36,13 @@
             <b-dropdown-item :to="`/memberships/${membership.id}/withdrawals`"> Withdrawals </b-dropdown-item>
             <b-dropdown-item :to="`/memberships/${membership.id}/promotions`"> Promotions </b-dropdown-item>
             <b-dropdown-divider />
-            <b-dropdown-item class="text-danger" @click="toggleDeleteModal()"> Remove </b-dropdown-item>
+            <b-dropdown-item v-b-modal="`modalDeleteMembership-${membership.id}`" class="text-danger">
+                Remove
+            </b-dropdown-item>
         </b-dropdown>
-        <base-modal-deposit-pool :membership="membership" />
+        <base-modal-deposit-pool v-if="membership.token" :membership="membership" />
+        <modal-delete :id="`modalDeleteMembership-${membership.id}`" :call="remove" :subject="membership.id" />
+
         <b-modal
             @ok="remove()"
             :visible="deleting"
@@ -47,10 +63,12 @@ import { UserProfile } from '@/store/modules/account';
 import { IMemberships, Membership } from '@/store/modules/memberships';
 import { WithdrawalState } from '@/store/modules/withdrawals';
 import BaseModalDepositPool from './modals/ModalDepositPool.vue';
+import ModalDelete from './modals/ModalDelete.vue';
 
 @Component({
     components: {
         BaseModalDepositPool,
+        ModalDelete,
     },
     computed: mapGetters({
         profile: 'account/profile',
@@ -84,16 +102,17 @@ export default class BaseListGroupItemAssetPool extends Vue {
         this.$store
             .dispatch('memberships/get', this.membership.id)
             .then(async ({ membership }: { membership: Membership; error: Error }) => {
-                if (!membership) return;
-                this.$store
-                    .dispatch('withdrawals/filter', {
-                        profile: this.profile,
-                        membership,
-                        state: WithdrawalState.Pending,
-                    })
-                    .then(({ pagination }) => {
-                        this.pendingWithdrawalCount = pagination?.total;
-                    });
+                if (membership) {
+                    this.$store
+                        .dispatch('withdrawals/filter', {
+                            profile: this.profile,
+                            membership,
+                            state: WithdrawalState.Pending,
+                        })
+                        .then(({ pagination }) => {
+                            this.pendingWithdrawalCount = pagination?.total;
+                        });
+                }
             });
     }
 }
