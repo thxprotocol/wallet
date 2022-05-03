646ece665f7b02cad58d585636e60719bcd37525
wooyh <yung.hui.woo@hva.nl>
2022-05-03 22:03:35 +0200

    Added ABI, added methodfor transfer

--- src/views/Claims.vue
+++ src/views/Claims.vue
@@ -1,22 +1,29 @@
 <template>
     <div>
-        <b-container class="bv-example-row">
-            <b-row class="mb-2">
+        <b-container>
+            <b-row class="mb-4">
                 <b-col>{{ isConnected ? 'You are connected with wallet: ' + account : 'Not connected' }}</b-col>
                 <b-col>
                     <b-button class="float-right" @click="connect" :hidden="isConnected">Connect metamask</b-button>
                 </b-col>
             </b-row>
 
-            <b-row class="mb-2" v-if="isConnected && getCurrentAmountOfTokens() > 0">
-                <b-col>{{ 'You have ' + getCurrentAmountOfTokens() + ' tokens to claim' }}</b-col>
+            <b-row class="mb-4" v-if="isConnected && getCurrentAmountOfTokens().length > 0">
+                <b-col>{{ 'You have ' + getCurrentAmountOfTokens().length + ' tokens to claim' }}</b-col>
                 <b-col>
-                    <b-button class="float-right">Claim all tokens</b-button>
+                    <b-button class="float-right" @click="payAllRewards">Claim all tokens</b-button>
                 </b-col>
             </b-row>
-            <b-row class="mb-2" v-else-if="isConnected">
+
+            <b-row class="mb-4" v-else-if="isConnected">
                 <b-col>{{ 'You have no tokens to claim' }}</b-col>
             </b-row>
+
+            <div class="mb-4" v-if="isConnected && getCurrentAmountOfTokens().length > 0">
+                <b-row class="mb-4" v-for="item in token" :key="item.name">
+                    {{ item.name }}
+                </b-row>
+            </div>
         </b-container>
     </div>
 </template>
@@ -26,7 +33,7 @@ import { Component, Vue } from 'vue-property-decorator';
 import { mapGetters, mapState } from 'vuex';
 import Web3 from 'web3';
 // TODO: insert abi from our smart contract
-import { default as ABIThx } from '@thxnetwork/artifacts/dist/exports/abis/ABITHX.json';
+import { default as ABIThx } from '../json/ABITHX.json';
 import { Contract } from 'web3-eth-contract';
 
 @Component({
@@ -36,15 +43,24 @@ export default class Claims extends Vue {
     account!: string;
     web3!: Web3;
     contract!: Contract;
+    token!: any;
 
     async connect() {
-        this.$store.dispatch('metamask/connect');
-        // TODO: insert abi from our smart contract
-        this.contract = new this.web3.eth.Contract(ABIThx as any, this.account as any);
+        await this.$store.dispatch('metamask/connect');
+        this.contract = new this.web3.eth.Contract(ABIThx as any, this.account);
     }
 
     async getCurrentAmountOfTokens() {
-        return await this.contract.methods.getAttempts().call();
+        this.token = await this.contract.methods.getRewards().call();
+        return this.token;
+    }
+
+    async payAllRewards() {
+        await this.contract.methods.withdrawal().call();
+    }
+
+    async payOneReward() {
+      //await this.contract.methods
     }
 
     mounted() {
