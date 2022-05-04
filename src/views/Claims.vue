7f575b441bfe34c51cc06985958239a05e173750
wooyh <yung.hui.woo@hva.nl>
2022-05-04 19:06:15 +0200

    added interface for object array

--- src/views/Claims.vue
+++ src/views/Claims.vue
@@ -8,23 +8,19 @@
                 </b-col>
             </b-row>
 
-            <b-row class="mb-4" v-if="isConnected && reward > 0">
-                <b-col>{{ 'You have ' + reward + ' tokens to claim' }}</b-col>
-                <b-col>
-                    <b-button class="float-right" @click="payAllRewards">Claim all tokens</b-button>
-                </b-col>
-            </b-row>
+            <template v-if="isConnected">
+                <b-row class="mb-4" v-if="reward > 0">
+                    <b-col>{{ 'You have ' + tokenAndAmount.length + ' token(s) to claim' }}</b-col>
+                    <b-col>
+                        <b-button class="float-right" @click="payAllRewards">Claim all tokens</b-button>
+                    </b-col>
+                </b-row>
 
-            <b-row class="mb-4" v-else-if="isConnected">
-                <b-col>{{ 'You have no tokens to claim' }}</b-col>
-            </b-row>
+                <b-row class="mb-4" v-else>
+                    <b-col>{{ 'You have no tokens to claim' }}</b-col>
+                </b-row>
 
-<!--            <div class="mb-4" v-if="isConnected && reward > 0">-->
-<!--                <b-row class="mb-4" v-for="item in tokenAndAmount" :key="item.token, item.amount">-->
-<!--                    {{ item.token }}-->
-<!--                    {{ item.amount }}-->
-<!--                </b-row>-->
-<!--            </div>-->
+            </template>
         </b-container>
     </div>
 </template>
@@ -40,43 +36,62 @@ import BigNumber from 'bignumber.js';
 @Component({
     computed: { ...mapState('metamask', ['account', 'chainId']), ...mapGetters('metamask', ['isConnected']) },
 })
+
 export default class Claims extends Vue {
     account!: string;
     web3!: Web3;
     contract!: Contract;
     reward!: any;
-    tokenAndAmount!: {token: string, amount: number}[];
+    tokenAndAmount: Token[] = [];
+
 
     async connect() {
-        const address = "0x5E0A87862f9175493Cc1d02199ad18Eff87Eb400"
+        const address = '0x5E0A87862f9175493Cc1d02199ad18Eff87Eb400';
         await this.$store.dispatch('metamask/connect');
         this.contract = new this.web3.eth.Contract(ABI_THX as any, address);
+        this.updateReward();
     }
 
     async updateReward() {
         const positionAddress = 0;
         const positonAmount = 2;
         let _amount!: any;
+        let token!: any;
         this.reward = 0;
-        const response = await this.contract.methods.getRewards().call();
-
-
-      for (let i = 0; i < response.length; i++) {
-            _amount = Number(response[i][positonAmount]._hex.toString())
-            this.reward += _amount;
-            this.tokenAndAmount.push([{token:response[i][positionAddress].toString(), amount: _amount }])
+        try {
+            const response = await this.contract.methods.getRewards().call();
+            for (let i = 0; i < response.length; i++) {
+                _amount = Number(response[i][positonAmount]._hex.toString());
+                this.reward += _amount;
+              console.log(response[i][positionAddress].toString())
+              console.log(_amount)
+                this.tokenAndAmount.push({ token: response[i][positionAddress].toString(), amount: _amount });
+            }
+        } catch (err) {
+            console.log('Error: ' + err);
         }
-        console.log('reward: ' + this.reward);
     }
 
     async payAllRewards() {
+        await this.contract.methods.withdraw().call();
+    }
+
+    async payOneReward() {
         await this.contract.methods.withdrawBulk().call();
     }
 
     mounted() {
-        // set to hardhat provider
+        // web3 set to hardhat provider
         this.web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545'));
         this.updateReward();
     }
+
+
 }
+
+interface Token {
+  token: string;
+  amount: number;
+}
+
 </script>
