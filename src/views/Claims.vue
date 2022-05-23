04893d0f61198acad4f2a38273ff3a132e9ef30b
yunghuiwoo <yung.hui.woo@hva.nl>
2022-05-23 20:53:17 +0200

    Add api call

--- src/views/Claims.vue
+++ src/views/Claims.vue
@@ -58,14 +58,11 @@ export default class Claims extends Vue {
         // set address of smart-contract, found in modules-solidity after command npx hardhat node
         await this.$store.dispatch('metamask/connect');
         this.contract = new this.web3.eth.Contract(feeCollectorAbi as AbiItem[], FEE_COLLECTOR_ADDRESS);
-        // when connected trough metamask update reward variable
-        const walletExist = await axios({
-            method: 'post',
-            url: getWalletUrl,
-            data: {
-                wallet: this.account,
-            },
-        });
+        // when connected trough metamask update reward variable;
+        const walletExist = await axios.get(getWalletUrl, {params: {
+            wallet: this.account
+          }} )
+        console.log(walletExist.data);
         this.updateReward();
     }
     /**
