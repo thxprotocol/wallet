b1529f025b4ca8bb71d16abac2a71540f160c7d0
yunghuiwoo <yung.hui.woo@hva.nl>
2022-05-26 13:53:27 +0200

    Remove extra whitespace

--- src/utils/secrets.ts
+++ src/utils/secrets.ts
@@ -2,7 +2,7 @@ export const API_ROOT = process.env['VUE_APP_API_ROOT'] || '';
 export const AUTH_URL = process.env['VUE_APP_AUTH_URL'] || '';
 export const BASE_URL = process.env['VUE_APP_BASE_URL'] || '';
 export const DASHBOARD_URL = process.env['VUE_APP_DASHBOARD_URL'] || '';
-export const VUE_APP_API_URL =  process.env['VUE_APP_API_URL'] || ''
+export const VUE_APP_API_URL = process.env['VUE_APP_API_URL'] || '';
 
 export const OIDC_CLIENT_ID = process.env['VUE_APP_OIDC_CLIENT_ID'] || '';
 export const OIDC_CLIENT_SECRET = process.env['VUE_APP_OIDC_CLIENT_SECRET'] || '';
